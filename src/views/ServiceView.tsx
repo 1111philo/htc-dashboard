af3f4328f32a953ed1c286dd5dead8ab1ba5210f
Mike Sammartino <sammartino.mike@gmail.com>
2025-01-23 23:12:29 -0600

    render slot table, add edit service dialog

--- src/views/ServiceView.tsx
+++ src/views/ServiceView.tsx
@@ -1,124 +1,189 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { Button, Modal, Form, Table } from 'react-bootstrap'
+import { Button, Modal, Form, Table } from "react-bootstrap";
 
-import { guests } from '../../sample-data/get_guests'
+import { guests } from "../../sample-data/get_guests";
 
 const ServiceView = ({ service }) => {
-  const [showEditModal, setShowEditModal] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false);
 
-  return (<>
-    {/*
-      Title: Service Name
-      Button (ADMIN): Edit Service - trigger edit modal
-      Table (QUOTA ONLY): Slots
-        fields: slot num, Guest Name, Buttons: Move to Completed, Move to Queue
-      Table: Queue
-        search / filter
-        fields: time requested, guest name w/ ID, Button: Move to Completed
-      Table Complete
-        search / filter
-        fields: time requested, guest name w/ ID, Button: Move to Queue
-    */}
-    <h1>Service Name</h1>
-    <Button onClick={ () => setShowEditModal(true) }>Edit Service</Button>
-    <Modal show={ showEditModal }>
+  const handleMoveToCompleted = () => {};
 
-    </Modal>
-    { true ?
-      (<>
-        <h2>Slots</h2>
-        <Table responsive={true}>
-          {/* this is a table of guests that have services with services[i].status === "Active" */}
-          <thead>
-            <tr>
-              <th>Slot #</th>
-              <th>Guest Name (ID)</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Every row: Slot number (iteration) */}
-            <tr>
-              <td>Slot #</td>
-              <td>Guest Name</td>
-              <td>Status</td>
-              <td>Actions</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </>)
-      : ('')
-    }
-    <h2>Queue</h2>
-    <Table responsive={true}>
-      <thead>
-        <tr>
-          <th>Time Requested</th>
-          <th>Guest Name (ID)</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* Every row:
-        guests[i].services[i].queued_at,
-        guests[i].first_name + last_name + guest_id
-        Button
-        */}
-        { guests.map(({ guest_id, first_name, last_name, services }, i) => {
-        let nameAndID = first_name + ' ' + last_name + ` (${guest_id})`
-        let completedServices = services.filter((service) => service.status === "Queued")
-        return completedServices.map((completeService, i) => {
-          return (
-            <tr key={i}>
-              <td>{ completeService.queued_at }</td>
-              <td>{ nameAndID }</td>
-              <td><Button>Test</Button></td>
-            </tr>
-          )
-        })
-      })}
-      </tbody>
-    </Table>
-    <h2>Completed</h2>
-    <Table responsive={true}>
-      <thead>
-        <tr>
-          <th>Time Requested</th>
-          <th>Guest Name (ID)</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-      { guests.map(({ guest_id, first_name, last_name, services }, i) => {
-        let nameAndID = first_name + ' ' + last_name + ` (${guest_id})`
-        let completedServices = services.filter((service) => service.status === "Completed")
-        return completedServices.map((completeService, i) => {
-          return (
-            <tr key={i}>
-              <td>{ completeService.queued_at }</td>
-              <td>{ nameAndID }</td>
-              <td><Button>Test</Button></td>
-            </tr>
-          )
-        })
-      })}
-      </tbody>
-    </Table>
-  </>)
-}
+  const handleMoveToQueue = () => {};
 
-export default ServiceView
+  const handleMoveToActive = () => {
+    // if not slot tracking: check if number of active guests is less than service.quota
+  };
+
+  return (
+    <>
+      <h1>Service Name</h1>
+      <Button onClick={() => setShowEditModal(true)}>Edit Service</Button>
+      <Modal show={showEditModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Service</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="serviceName">
+              <Form.Control type="email" defaultValue={"Service name"} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="serviceQuota">
+              <Form.Control type="number" defaultValue={10} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+      {true /* service.quota */ ? (
+        <>
+          <h2>Slots</h2>
+          <Table responsive={true}>
+            <thead>
+              <tr>
+                <th>Slot #</th>
+                <th>Guest Name (ID)</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Every row (iteration: where services[i].service_name matches === shower, services.quota num of times) */}
+              {
+                // if guest has an active service
+                //    render with guest info
+                // else
+                //    render with placeholders
+                // service.quota - quests.length
+
+                guests.map(
+                  (
+                    { guest_id, first_name, last_name, services },
+                    guestIndex
+                  ) => {
+                    const nameAndID =
+                      first_name + " " + last_name + ` (${guest_id})`;
+                    const activeServices = services.filter(
+                      (service) => service.status === "Active"
+                    );
+                    return activeServices.map((activeService, serviceIndex) => {
+                      return (
+                        <tr key={serviceIndex}>
+                          <td>{guestIndex + 1}</td>
+                          <td>{nameAndID}</td>
+                          <td>Occupied</td>
+                          <td>
+                            <Button onClick={handleMoveToCompleted}>
+                              Move to Completed
+                            </Button>
+                            <Button onClick={handleMoveToQueue}>
+                              Move to Queue
+                            </Button>
+                          </td>
+                        </tr>
+                      );
+                    });
+                  }
+                )
+              }
+              {Array.from({ length: 10 }).map((_, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>Guest Name (ID)</td>
+                  <td>Status</td>
+                  <td>
+                    <Button onClick={handleMoveToCompleted}>
+                      Move to Completed
+                    </Button>
+                    <Button onClick={handleMoveToQueue}>Move to Queue</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      ) : (
+        ""
+      )}
+      <h2>Queue</h2>
+      <Table responsive={true}>
+        <thead>
+          <tr>
+            <th>Time Requested</th>
+            <th>Guest Name (ID)</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guests.map(({ guest_id, first_name, last_name, services }) => {
+            const nameAndID = first_name + " " + last_name + ` (${guest_id})`;
+            const queuedServices = services.filter(
+              (service) => service.status === "Queued"
+            );
+            return queuedServices.map((queuedService, i) => {
+              return (
+                <tr key={i}>
+                  <td>{queuedService.queued_at}</td>
+                  <td>{nameAndID}</td>
+                  <td>
+                    {true /* service.quota */ ? (
+                      <Form.Select aria-label="Select which slot to assign">
+                        <option>Assign Slot</option>
+                        {Array.from({ length: 10 }).map((_, i) => {
+                          // TODO: need array of available slots for these options
+                          return <option key={i}>{i + 1}</option>;
+                        })}
+                      </Form.Select>
+                    ) : (
+                      ""
+                    )}
+                    <Button onClick={handleMoveToCompleted}>
+                      Move to Completed
+                    </Button>
+                  </td>
+                </tr>
+              );
+            });
+          })}
+        </tbody>
+      </Table>
+      <h2>Completed</h2>
+      <Table responsive={true}>
+        <thead>
+          <tr>
+            <th>Time Requested</th>
+            <th>Guest Name (ID)</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guests.map(({ guest_id, first_name, last_name, services }) => {
+            const nameAndID = first_name + " " + last_name + ` (${guest_id})`;
+            const completedServices = services.filter(
+              (service) => service.status === "Completed"
+            );
+            return completedServices.map((completeService, i) => {
+              return (
+                <tr key={i}>
+                  <td>{completeService.queued_at}</td>
+                  <td>{nameAndID}</td>
+                  <td>
+                    <Button onClick={handleMoveToQueue}>Move to Queue</Button>
+                  </td>
+                </tr>
+              );
+            });
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default ServiceView;
