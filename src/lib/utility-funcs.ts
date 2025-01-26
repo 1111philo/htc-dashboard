8da3ad0eeb0785ae322b553de1ed729a7d716969
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-01-26 12:47:54 -0600

    add: sortable, filterable table; refactor: factor out StripedListRow; move: shared func to utils; add: dep lucid-react; add: type GuestAPIResponse; change: tsconfig: newer ES spec to accommodate String.padStart()

--- src/lib/utility-funcs.ts
+++ src/lib/utility-funcs.ts
@@ -1,5 +1,10 @@
-// UTIL
+/** Shared Utility Functions */
+
 /** Return today's date in YYYY/MM/DD format. */
 export function today(): string {
   return new Date().toISOString().split("T")[0];
+}
+
+export function isEven(num) {
+  return (num & 1) === 0;
 }
