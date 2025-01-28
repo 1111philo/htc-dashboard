157dfd8fb6a16d686307052756554fc74f33e798
Mike Sammartino <sammartino.mike@gmail.com>
2025-01-27 18:05:46 -0600

    chart component init

--- src/lib/components/Chart.tsx
+++ src/lib/components/Chart.tsx
@@ -1,4 +1,3 @@
-import React, { PureComponent } from "react";
 import {
   BarChart,
   Bar,
@@ -56,40 +55,37 @@ const data = [
   },
 ];
 
-export default class Chart extends PureComponent {
-  static demoUrl = "https://codesandbox.io/p/sandbox/simple-bar-chart-72d7y5";
+export default function Chart() {
 
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar
-            dataKey="pv"
-            fill="#8884d8"
-            activeBar={<Rectangle fill="pink" stroke="blue" />}
-          />
-          <Bar
-            dataKey="uv"
-            fill="#82ca9d"
-            activeBar={<Rectangle fill="gold" stroke="purple" />}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-    );
-  }
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="pv"
+          fill="#8884d8"
+          activeBar={<Rectangle fill="pink" stroke="blue" />}
+        />
+        <Bar
+          dataKey="uv"
+          fill="#82ca9d"
+          activeBar={<Rectangle fill="gold" stroke="purple" />}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 }
