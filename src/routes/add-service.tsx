73e57eab346471e2eedb4d06b25600503f20a6da
Mike Sammartino <sammartino.mike@gmail.com>
2025-01-29 20:54:42 -0600

    add service view with live api and basic validation

--- src/routes/add-service.tsx
+++ src/routes/add-service.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useLoaderData, useNavigate } from '@tanstack/react-router';
+import * as API from 'aws-amplify/api';
+import { fetchServices } from '../lib/api';
+import FeedbackMessage from '../lib/components/FeedbackMessage';
 
-import * as API from "aws-amplify/api";
-
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 export const Route = createFileRoute("/add-service")({
   component: AddServiceView,
+  loader: async () => {
+    const services = await fetchServices();
+    return { services }
+  }
 });
 
 function AddServiceView() {
@@ -19,10 +24,35 @@ function AddServiceView() {
 }
 
 function AddNewServiceForm() {
+  const { services } = Route.useLoaderData()
+  const navigate = useNavigate();
+
   const [newServiceName, setNewServiceName] = useState("")
-  const [optionalQuota, setOptionalQuota] = useState("")
+  const [optionalQuota, setOptionalQuota] = useState(0)
+  const [feedback, setFeedback] = useState<UserMessage>({
+    text: "",
+    isError: false,
+  });
 
   const handleCreateNewService = async () => {
+    // check if new service has a name
+    if (!newServiceName) {
+      setFeedback({
+        text: "Service must be named",
+        isError: true
+      })
+      return;
+    }
+    // check if new service is unique
+    let duplicateService = services.some((service) => service.name === newServiceName)
+    if (duplicateService) {
+      setFeedback({
+        text: "Service already exists.",
+        isError: true
+      })
+      return;
+    }
+
     // send new service name and quota to api
     const response = await (
       await API.post({
@@ -35,34 +65,56 @@ function AddNewServiceForm() {
           }
         }
       }).response
-    ).statusCode
-    console.log("response from api", response)
+    )
+
+    if (response!.statusCode === 200) {
+      setFeedback({ text: 'Success', isError: false })
+      const newService = (await response.body.json())
+      // route user to view for new service
+      navigate({ to: `/services/${newService!.service_id}` })
+    }
   }
 
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleCreateNewService()
+    }
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="serviceName">
-        <Form.Control
-          type="email"
-          placeholder="New Service Name"
-          onChange={(e) => setNewServiceName(e.target.value)}
-        />
-      </Form.Group>
+    <>
+      <FeedbackMessage
+        text={feedback.text}
+        isError={feedback.isError}
+        className="my-3"
+      />
 
-      <Form.Group className="mb-3" controlId="serviceQuota">
-        <Form.Control
-          type="number"
-          placeholder="Quota (optional)"
-          onChange={(e) => setOptionalQuota(e.target.value)}
-        />
-      </Form.Group>
+      <Form>
+        <Form.Group className="mb-3" controlId="serviceName">
+          <Form.Control
+            type="email"
+            placeholder="New Service Name"
+            onChange={(e) => setNewServiceName(e.target.value)}
+            onKeyDown={handleEnter}
+          />
+        </Form.Group>
 
-      <Button
-        variant="primary"
-        onClick={handleCreateNewService}
-      >
-        Create New Service
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="serviceQuota">
+          <Form.Control
+            type="number"
+            placeholder="Quota (optional)"
+            onChange={(e) => setOptionalQuota(parseInt(e.target.value))}
+            onKeyDown={handleEnter}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          onClick={handleCreateNewService}
+        >
+          Create New Service
+        </Button>
+      </Form>
+    </>
   );
 }
