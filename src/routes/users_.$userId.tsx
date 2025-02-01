b2e4ed6d2469aabe82e93153be82e978223a808c
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-01-31 21:15:54 -0600

    add: edit & delete user

--- src/routes/users_.$userId.tsx
+++ src/routes/users_.$userId.tsx
@@ -1,7 +1,8 @@
-import { createFileRoute, useParams } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { getUser } from "../lib/api/user";
+import FeedbackMessage from "../lib/components/FeedbackMessage2";
+import { deleteUser, getUser, updateUser } from "../lib/api/user";
 
 interface LoaderData {
   user: User;
@@ -12,176 +13,202 @@ export const Route = createFileRoute("/users_/$userId")({
     userId: parseInt(params.userId),
   }),
   loader: async ({ params }): Promise<LoaderData> => {
-    const { userId } = params
+    const { userId } = params;
     // TODO: fix the hack in 👇🏽 this request func when API accepts a user_id key in req body
-    const user = await getUser(userId)
-    return { user: user! }
-  }
+    const user = await getUser(userId);
+    return { user: user! };
+  },
 });
 
 function UserProfileView() {
-  const { user } = Route.useLoaderData()
+  const { user } = Route.useLoaderData();
+
+  const navigate = useNavigate();
+
+  const blankFeedback: UserMessage = { text: "", isError: false };
+  const [feedback, setFeedback] = useState(blankFeedback);
 
   return (
     <>
-      <h1>Staff Profile</h1>
-      <UserForm />
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Staff Profile</h1>
+        <Button
+          variant="danger"
+          type="button"
+          onClick={async () => await deleteUsr()}
+        >
+          Delete User
+        </Button>
+      </div>
+      <FeedbackMessage message={feedback} />
+      <UserForm user={user} setFeedback={setFeedback} />
     </>
   );
 
-  function UserForm() {
-    const initialFields: Partial<User> = {
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    };
+  async function deleteUsr() {
+    if (
+      !confirm(
+        `Are you sure you want to delete ${user.role}:
+          ${user.email}`
+      )
+    ) {
+      return;
+    }
+    const success = await deleteUser(user.user_id);
+    if (!success) {
+      setFeedback({
+        text: `Oops! The user couldn't be deleted. Try again in a few.`,
+        isError: true,
+      });
+      return;
+    }
+    navigate({ to: "/users", replace: true });
+  }
+}
 
-    const [fields, setFields] = useState(initialFields);
+function UserForm({ user, setFeedback }) {
+  const initialFields: Partial<User> = {
+    name: user.name ?? "",
+    email: user.email ?? "",
+    role: user.role ?? "",
+  };
+  const [fields, setFields] = useState(initialFields);
 
-    const [password, setPassword] = useState({ password: "", confirm: "" });
-    const [errorMsg, setErrorMsg] = useState("");
+  const [password, setPassword] = useState({ password: "", confirm: "" });
 
-    const isPasswordChanged = password.password !== "";
-    const isFormChanged =
-      user.email !== fields.email ||
-      user.role !== fields.role ||
-      isPasswordChanged;
+  const isPasswordChanged = password.password !== "";
+  const isFormChanged =
+    user.name !== fields.name ||
+    user.email !== fields.email ||
+    user.role !== fields.role ||
+    isPasswordChanged;
 
-    return (
-      <Form onSubmit={saveEditedUser}>
-        <p className="text-danger">{errorMsg}</p>
-        <h4>ID: {user.user_id.toString().padStart(5, "0")}</h4> 
-        <Form.Group className="mb-3">
-          <Form.Label className="fst-italic">Name (First Last)</Form.Label>
-          <Form.Control
-            id="input-name"
-            name="name"
-            value={fields.name}
-            onChange={(e) => setFields({ ...fields, name: e.target.value })}
-            size="lg"
-            className={
-              fields.name?.trim() !== user.name
-                ? "border-2 border-warning"
-                : ""
-            }
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="fst-italic">Email</Form.Label>
-          <Form.Control
-            id="input-email"
-            name="email"
-            type="email"
-            value={fields.email}
-            onChange={(e) => setFields({ ...fields, email: e.target.value })}
-            className={
-              fields.email?.trim() !== user.email
-                ? "border-2 border-warning"
-                : ""
-            }
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="fst-italic">Role</Form.Label>
-          <Form.Select
-            id="input-role"
-            name="role"
-            value={fields.role}
-            onChange={(e) => setFields({ ...fields, role: e.target.value })}
-            className={
-              fields.role?.trim() !== user.role ? "border-2 border-warning" : ""
-            }
-          >
-            <option value="manager">Manager</option>
-            <option value="admin">Admin</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="fst-italic">New Password</Form.Label>
-          <Form.Control
-            id="input-password"
-            name="password"
-            type="password"
-            minLength={8}
-            maxLength={33}
-            value={password.password}
-            onChange={(e) =>
-              setPassword({ ...password, password: e.target.value })
-            }
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="fst-italic">Confirm Password</Form.Label>
-          <Form.Control
-            id="input-confirm-password"
-            name="confirm-password"
-            type="password"
-            value={password.confirm}
-            onChange={(e) =>
-              setPassword({ ...password, confirm: e.target.value })
-            }
-          />
-        </Form.Group>
+  return (
+    <Form onSubmit={saveEditedUser}>
+      <h4>ID: {user.user_id.toString().padStart(5, "0")}</h4>
+      <Form.Group className="mb-3">
+        <Form.Label className="fst-italic">Full Name</Form.Label>
+        <Form.Control
+          id="input-name"
+          name="name"
+          value={fields.name}
+          onChange={(e) => setFields({ ...fields, name: e.target.value })}
+          size="lg"
+          className={
+            fields.name?.trim() !== user.name ? "border-2 border-warning" : ""
+          }
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label className="fst-italic">Email</Form.Label>
+        <Form.Control
+          id="input-email"
+          name="email"
+          type="email"
+          value={fields.email}
+          onChange={(e) => setFields({ ...fields, email: e.target.value })}
+          className={
+            fields.email?.trim() !== user.email ? "border-2 border-warning" : ""
+          }
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label className="fst-italic">Role</Form.Label>
+        <Form.Select
+          id="input-role"
+          name="role"
+          value={fields.role}
+          onChange={(e) => setFields({ ...fields, role: e.target.value })}
+          className={
+            fields.role?.trim() !== user.role ? "border-2 border-warning" : ""
+          }
+        >
+          <option value="manager">Manager</option>
+          <option value="admin">Admin</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label className="fst-italic">New Password</Form.Label>
+        <Form.Control
+          id="input-password"
+          name="password"
+          type="password"
+          minLength={6}
+          maxLength={33}
+          readOnly // this + onFocus = hack to stop autofilling of password
+          onFocus={(e) => e.target.removeAttribute("readonly")}
+          value={password.password}
+          onChange={(e) =>
+            setPassword({ ...password, password: e.target.value })
+          }
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label className="fst-italic">Confirm Password</Form.Label>
+        <Form.Control
+          id="input-confirm-password"
+          name="confirm_password"
+          type="password"
+          readOnly // this + onFocus = hack to stop autofilling of password
+          onFocus={(e) => e.target.removeAttribute("readonly")}
+          value={password.confirm}
+          onChange={(e) =>
+            setPassword({ ...password, confirm: e.target.value })
+          }
+        />
+      </Form.Group>
+      {isFormChanged && (
         <div className="d-flex gap-2 justify-content-between">
-          <Button
-            variant="danger"
-            type="button"
-            onClick={async () => await deleteUser()}
-          >
-            Delete Guest
+          <Button variant="warning" type="button" onClick={cancelEdit}>
+            Discard Changes
           </Button>
-          {isFormChanged && (
-            <div className="d-flex gap-2">
-              <Button variant="warning" type="button" onClick={cancelEdit}>
-                Cancel
-              </Button>
-              <Button variant="primary" type="submit">
-                Save Changes
-              </Button>
-            </div>
-          )}
+          <Button variant="primary" type="submit">
+            Save Changes
+          </Button>
         </div>
-      </Form>
-    );
+      )}
+    </Form>
+  );
 
-    function cancelEdit() {
+  function cancelEdit() {
+    setFields(initialFields);
+    setPassword({ password: "", confirm: "" });
+  }
+
+  async function saveEditedUser(e: SubmitEvent, onSave) {
+    e.preventDefault();
+    // first, check if passwords match
+    if (password.password.trim() !== password.confirm.trim()) {
+      setFeedback({ text: "Passwords don't match.", isError: true });
+
+      return;
+    } else {
+      setFeedback({ text: "", isError: false });
+    }
+    if (
+      !confirm(`Save changes?
+        ${user.email} -> ${fields.email}
+        ${user.role} -> ${fields.role}`)
+    ) {
+      return;
+    }
+
+    const userWithPassword = Object.fromEntries(
+      new FormData(e.target)
+    ) as Partial<User> & { password: string; confirm_password: string };
+    const { confirm_password, ...rest } = userWithPassword;
+    const updatedUser = { ...rest, user_id: user.user_id };
+    const success = await updateUser(updatedUser); // placeholder
+    if (!success) {
+      setFeedback({
+        text: "Oops! The edits couldn't be saved. Try again in a few.",
+        isError: true,
+      });
       setFields(initialFields);
       setPassword({ password: "", confirm: "" });
+      return;
     }
-
-    async function deleteUser() {
-      if (
-        !confirm(
-          `Are you sure you want to delete ${user.role}:
-          ${user.email}`
-        )
-      ) {
-        return;
-      }
-      // delete the user
-    }
-
-    async function saveEditedUser(evt: SubmitEvent) {
-      evt.preventDefault();
-      // first, check if passwords match
-      if (password.password.trim() !== password.confirm.trim()) {
-        setErrorMsg("❗ Passwords don't match.");
-        return;
-      } else {
-        setErrorMsg("");
-      }
-      if (
-        !confirm(`Save changes?
-          ${user.email} -> ${fields.email}
-          ${user.role} -> ${fields.role}`)
-      ) {
-        return;
-      }
-      // TODO: fetch/POST new guest
-      const { success } = { success: true }; // placeholder
-      if (success) {
-        // TODO: report success with a toast (or anything, for now)
-      }
-    }
+    setFeedback({ text: "Successfully updated.", isError: false });
+    setPassword({ password: "", confirm: "" });
   }
 }
