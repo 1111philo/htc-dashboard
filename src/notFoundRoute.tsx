505c9b2531ed7e0f9c0ed7f0579cbbe2848b4c80
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-02-03 19:13:34 -0600

    add: proper not found view

--- src/notFoundRoute.tsx
+++ src/notFoundRoute.tsx
@@ -1,13 +1,37 @@
-import { NotFoundRoute } from "@tanstack/react-router";
+import { Link, NotFoundRoute } from "@tanstack/react-router";
 import { Route as rootRoute } from "./routes/__root";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 export const notFoundRoute = new NotFoundRoute({
   getParentRoute: () => rootRoute,
-  component: NotFound404
+  component: NotFound404,
 });
 
 function NotFound404() {
   return (
-    <div>[Customize Me] 404 Not Found [Customize Me]</div>
-  )
+    <Container>
+      <Row className="py-5 align-items-center justify-content-center">
+        <Col md={8} className="text-center">
+          <h1 className="display-1 fw-bold">404</h1>
+          <div className="mb-4">
+            <div className="h2">Page Not Found</div>
+            <p className="lead text-muted">
+              The page you are looking for might have been removed, had its name
+              changed, or is temporarily unavailable.
+            </p>
+          </div>
+          <Button
+            variant="primary"
+            size="lg"
+            className="text-decoration-none"
+            as={Link}
+            to={".."}
+            replace={true}
+          >
+            Go Back
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
