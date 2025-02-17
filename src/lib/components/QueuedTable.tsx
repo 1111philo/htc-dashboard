f0158ba53ed086be2bd1b1087945c4dc27b209e7
Mike Sammartino <sammartino.mike@gmail.com>
2025-02-17 15:19:09 -0600

    remove guest id from queued table, make link to guest profile more obvious

--- src/lib/components/QueuedTable.tsx
+++ src/lib/components/QueuedTable.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { readableDateTime } from "../utils";
 import { updateGuestServiceStatus } from "../api";
@@ -101,7 +101,6 @@ export default function QueuedTable({
           <tr>
             <th>#</th>
             <th>Time Requested</th>
-            <th>Guest ID</th>
             <th>Guest Name</th>
             <th>Actions</th>
           </tr>
@@ -116,8 +115,11 @@ export default function QueuedTable({
                 <tr key={`${guest.guest_id}-${i}`}>
                   <td>{i + 1}</td>
                   <td>{timeRequested}</td>
-                  <td onClick={() => navigate({ to: `/guests/${guest.guest_id}` })}>{guest.guest_id}</td>
-                  <td onClick={() => navigate({ to: `/guests/${guest.guest_id}` })}>{fullName}</td>
+                  <td onClick={() => navigate({ to: `/guests/${guest.guest_id}` })}>
+                    <Link to="/guests/$guestId" params={{ guestId: guest.guest_id }}>
+                      {fullName}
+                    </Link>
+                  </td>
                   <td>
                     <div className="d-flex flex-column justify-content-end">
                       {service.quota ? (
