21848ffbff9aeee8e12c03f3c943b631973f5f26
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-02-25 12:00:44 -0600

    complete op started in last commit

--- src/routes/_auth._admin.users_.$userId.tsx
+++ src/routes/_auth._admin.users_.$userId.tsx
@@ -1,27 +1,27 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { UserProfile } from "../lib/components";
-import { getUser } from "../lib/api";
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { UserProfile } from '../lib/components'
+import { getUser } from '../lib/api'
 
 interface LoaderData {
-  user: User;
-  isOwnAccount: boolean;
+  user: User
+  isOwnAccount: boolean
 }
-export const Route = createFileRoute("/_auth/_admin/users_/$userSub")({
+export const Route = createFileRoute('/_auth/_admin/users_/$userId')({
   component: UserProfileView,
   parseParams: (params): { userSub: string } => ({
     userSub: params.userSub,
   }),
   loader: async ({ context, params }): Promise<LoaderData> => {
-    const { userSub } = params;
+    const { userSub } = params
     // TODO: fix the hack in 👇🏽 this request func when API accepts a user_id key in req body
-    const user = await getUser(userSub);
-    if (!user) throw redirect({ to: "/users", replace: true });
-    const isOwnAccount = context.authUser!.sub === user?.sub;
-    return { user, isOwnAccount };
+    const user = await getUser(userSub)
+    if (!user) throw redirect({ to: '/users', replace: true })
+    const isOwnAccount = context.authUser!.sub === user?.sub
+    return { user, isOwnAccount }
   },
-});
+})
 
 function UserProfileView() {
-  const { user, isOwnAccount } = Route.useLoaderData();
-  return <UserProfile user={user} isOwnAccount={isOwnAccount} />;
+  const { user, isOwnAccount } = Route.useLoaderData()
+  return <UserProfile user={user} isOwnAccount={isOwnAccount} />
 }
